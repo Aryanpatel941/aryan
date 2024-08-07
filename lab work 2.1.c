5c906b0a8c620e384117e91f45021e6e5b8beb22
patelsaryan941@gmail.com <patelsaryan941@gamil.com>
2024-08-07 12:36:30 +0530

    first commit

---
+++ lab work 2.1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	printf("%d", 12+6);
+	printf("\n%d", 12-6);
+	printf("\n%d",12*6);
+	printf("\n%d",12/6);
+	printf("\n%d",12%6);
+	printf("\n");
+	
+	
+
+    printf("\n");
+    printf("---------\n"); 
+	printf("|       |\n");
+	printf("| 5*5=25|\n");
+	printf("|       |\n");
+	printf("---------\n");
+	
+}
