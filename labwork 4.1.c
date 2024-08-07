0b3f36db4b4fb6e78daca1507e2f301d1177bf7b
patelsaryan941@gmail.com <patelsaryan941@gamil.com>
2024-08-07 12:03:31 +0530

    first commit

---
+++ labwork 4.1.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+main()
+{
+	/*
+	float x,y;
+	printf(" enter the value of x :");
+	scanf(" %f" , &x);
+	printf(" enter the value of y :");
+	scanf(" %f" , &y);
+	float result=(x + y)*(x + y);
+	printf(" the result of (x + y)*(x + y) is : %f\n\n\n ", result);
+	*/
+	
+	/*
+	float x,y;
+	printf(" enter the value of x :");
+	scanf(" %f" , &x);
+	printf(" enter the value of y :");
+	scanf(" %f" , &y);
+	float  result=(x - y)*(x - y);
+	printf(" the result of (x - y)*(x - y) is : %f ", result);
+	*/
+	
+	/*
+	float x,y;
+	printf(" enter the value of x :");
+	scanf(" %f" , &x);
+	printf(" enter the value of y :");
+	scanf(" %f" , &y);
+	float result=(x + y)*(x + y)*(x + y);
+	printf(" the result of (x + y)*(x + y)*(x + y) is : %f\n\n\n ", result);
+	*/
+	
+	/*
+	int a, b, c;
+	printf("Enter value for a: ");
+    scanf("%d", &a);  
+	printf("Enter value for b: ");
+    scanf("%d", &b); 
+    c = a;  
+    a = b;     
+    b = c;  
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    */
+    
+    
+    int a, b;
+	printf("Enter value for a: ");
+    scanf("%d", &a);  
+	printf("Enter value for b: ");
+    scanf("%d", &b); 
+	printf("Before swapping: a = %d, b = %d\n", a, b);  
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    
+
+
+    
+  
+
+
+}
+
+    
+	
+	
+
+
+    
+
+    
+
+
+
---
+++ logical.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int x=0;
+	int y=0;
+	int a=!x;
+	int b=!y;
+	int c=x&&y;
+	int d=x||y;
+	printf("%d,%d,%d,%d,",a,b,c,d);
+	
+}
+
